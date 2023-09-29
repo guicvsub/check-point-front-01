0653fad86b59a21c1cba6fc870ead00c4e0a3140
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 09:10:39 -0300

    esse e o primeiro cp de web

---
+++ script.js
@@ -0,0 +1,28 @@
+let nome = "guilherme Santiago da silva "
+let idade = 24
+let status = true 
+let  valorProduto = 10.50
+let listaTimes = ["corinthians", "santos", "bragantino", ]
+console.log(nome);
+console.log(idade)
+console.log(status)
+console.log(valorProduto)
+console.log(listaTimes)
+//operaçoes matematicas
+let num1 = 1
+let nun2 = 2
+let nun3 = 4
+let nun4 = 5
+let nun5 = 6
+//soma 
+let soma = num1 + nun2 + nun3 + nun4 + nun5;
+console.log("resultado soma = ", soma);
+// subtração 
+let = sub = num1 - nun2 - nun3 - nun4 - nun5;
+console.log("resultado da subtração", sub);
+// multiplicação 
+let multiplicação = num1 * nun2 * nun3 * nun4 * nun5;
+console.log("resultado da multiplicação" , multiplicação);
+// divisão
+let divisão = num1 / nun2 / nun3 / nun4 / nun5;
+console.log("resultado da divisao", divisão)
