bd32917215e3875b050029c78e54969d363695d0
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 09:38:25 -0300

    checkpoint 01

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+//declarando variaveis
 let nome = "guilherme Santiago da silva "
 let idade = 24
 let status = true 
 let  valorProduto = 10.50
 let listaTimes = ["corinthians", "santos", "bragantino", ]
+let num1 = 1
+let nun2 = 2
+let nun3 = 4
+let nun4 = 5
+let nun5 = 6
+//printando no console
 console.log(nome);
 console.log(idade)
 console.log(status)
 console.log(valorProduto)
 console.log(listaTimes)
 //operaçoes matematicas
-let num1 = 1
-let nun2 = 2
-let nun3 = 4
-let nun4 = 5
-let nun5 = 6
+
 //soma 
 let soma = num1 + nun2 + nun3 + nun4 + nun5;
 console.log("resultado soma = ", soma);
